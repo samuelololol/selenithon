37e8ff93923b7fd538e67308ff732356da4a38e6
samuelololol <samuelololol@gmail.com>
2018-01-08 01:06:13 +0800

    update

--- entry_point.sh
+++ entry_point.sh
@@ -7,24 +7,11 @@ rm -f /tmp/.X*lock
 # enable virtualenvwrapper
 source "/usr/local/bin/virtualenvwrapper.sh" > /dev/null 2>&1
 echo "enable virtualenvwrapper"
-# collect env variables
-export NAME=$(grep -o -P "(?<=VIRTUAL_ENV=\"\/Users\/)\w.+(?=\/\.)|(?<=VIRTUAL_ENV=\"\/home\/)\w.+(?=\/\.)" /root/.virtualenvs/env/bin/activate)
-export HOSTENVNAME=$(grep -o -P '(?<=\/.virtualenvs\/)\w+' /root/.virtualenvs/env/bin/activate)
-export SHELLNAME=$(head -n 1 /root/.virtualenvs/env/bin/preactivate | awk -F "/" '{print $NF}')
-export HOSTHOMEPATH=$(grep -o -P "(?<=\#\!).+(?=\/\.virtualenvs)" /root/.virtualenvs/env/bin/pip)
-#echo NAME:$NAME
-#echo HOSTENVNAME:$HOSTENVNAME
-#echo SHELLNAME:$SHELLNAME
 
-# sed host venv path settings
-#1. change to bash from zsh
-sed -i -e 's@'"$SHELLNAME"'@bash@g' /root/.virtualenvs/env/bin/preactivate 2>&1
-#2. change to root from user folder
-sed -i -e 's@VIRTUAL_ENV="\/home\/'"$NAME"'@VIRTUAL_ENV="\/root@g' /root/.virtualenvs/env/bin/activate
-sed -i -e 's@'"$HOSTHOMEPATH"'@\/root@g' /root/.virtualenvs/env/bin/pip
-#3. change env name to 'env'
-sed -i -e 's@\.virtualenvs\/'"$HOSTENVNAME"'@\.virtualenvs\/env@g' /root/.virtualenvs/env/bin/activate
-sed -i -e 's@\.virtualenvs\/'"$HOSTENVNAME"'@\.virtualenvs\/env@g' /root/.virtualenvs/env/bin/pip
+# debug
+echo "ENV_NAME_@ACTIVATE:" $(grep -o -P '(?<=\/.virtualenvs\/)\w+' /root/.virtualenvs/env/bin/activate)
+echo "HOME_NAME_@PIP:" $(grep -o -P "(?<=\#\!).+(?=\/\.virtualenvs)" /root/.virtualenvs/env/bin/pip)
+echo "SHELL_NAME_@PREACTIVATE:" $(head -n 1 /root/.virtualenvs/env/bin/preactivate | awk -F "/" '{print $NF}')
 
 # use env
 workon env
@@ -50,10 +37,3 @@ NODE_PID=$!
 
 #clean up
 #rm -rf geckodriver.log
-sed -i -e 's@bash@'"$SHELLNAME"'@g' /root/.virtualenvs/env/bin/preactivate
-sed -i -e 's@VIRTUAL_ENV="\/root@VIRTUAL_ENV="'"$HOSTHOMEPATH"'@g' /root/.virtualenvs/env/bin/activate
-sed -i -e 's@\/root@'"$HOSTHOMEPATH"'@g' /root/.virtualenvs/env/bin/pip
-sed -i -e 's@\.virtualenvs\/env@\.virtualenvs\/'"$HOSTENVNAME"'@g' /root/.virtualenvs/env/bin/activate
-sed -i -e 's@\.virtualenvs\/env@\.virtualenvs\/'"$HOSTENVNAME"'@g' /root/.virtualenvs/env/bin/pip
-chown ${USER_ID}:${USER_ID} /app/*
-echo "clean up, quit"
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker run --rm -t $(tty &>/dev/null && echo "-i") \
-        -e "SCREEN_WIDTH=${SCREEN_W}" \
-        -e "SCREEN_HEIGHT=${SCREEN_H}" \
-        -e "SCREEN_DEPTH=${SCREEN_D}" \
-        -v "/home/docker/.virtualenvs/venv-docker-machine:/root/.virtualenvs/env" \
-        -v "/app:/app" samuelololol/selenithon "$@"
-#        -v "/app:/app" dse:v0.19.1 "$@"
