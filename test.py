3645d0c72e2fd0e78a0482aeed5b12d75cce5434
samuelololol <samuelololol@gmail.com>
2018-11-07 22:06:05 +0800

    Disable marionette

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 # __author__ = 'samuel'
 # from selenium import webdriver
 from seleniumrequests import Firefox
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from xvfbwrapper import Xvfb
 
 
@@ -11,6 +12,8 @@ def main():
     with Xvfb(width=1280, height=2000):
         # driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
         # driver = webdriver.Firefox()
+        firefox_capabilities = DesiredCapabilities.FIREFOX
+        firefox_capabilities['marionette'] = False
         driver = Firefox()
         driver.get('https://tw.yahoo.com')
         driver.save_screenshot('screenshot.png')
